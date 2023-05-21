0e65be8d92bc8397c9cdd749876138f2c7fc59d6
Horizon <coquiaprinceangelo@gmail.com>
2023-05-21 19:35:56 +0800

    Edge Reward Auto Complete

---
+++ main.py
@@ -0,0 +1,18 @@
+import random
+import webbrowser
+
+def generate_random_word(length):
+    letters = 'abcdefghijklmnopqrstuvwxyz'
+    return ''.join(random.choice(letters) for _ in range(length))
+
+def search_random_word():
+    random_word = generate_random_word(random.randint(5, 10))
+    query = f"https://www.bing.com/search?q={random_word}"
+    webbrowser.open(query)
+
+# Prompt for the number of searches
+num_searches = int(input("Enter the number of random searches: "))
+
+# Perform random searches
+for _ in range(num_searches):
+    search_random_word()
